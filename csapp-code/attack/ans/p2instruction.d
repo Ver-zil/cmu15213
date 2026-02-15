a6210175339bdc47ddcbcdc61657b94dc9e33977
Verzil <1143538548@qq.com>
2026-02-15 11:40:39 +0000

    feat: complete phase2 of Attack Lab

---
+++ csapp-code/attack/ans/p2instruction.d
@@ -0,0 +1,9 @@
+
+./ans/p2instruction.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 fa 97 b9 59 	mov    $0x59b997fa,%rdi
+   7:	e8 00 00 00 00       	callq  0xc
